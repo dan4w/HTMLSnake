6ea411b31bfe0f2dd937051e687900ce4a810964
t3hdaniel <daniel4wood@hotmail.com>
2018-09-24 21:58:25 -0700

    Added comments for all files

--- controller.js
+++ controller.js
@@ -5,18 +5,20 @@
  * @Author: Daniel Forwood
  * @Date:   2018-06-29T19:13:18-07:00
  * @Last modified by:   Daniel Forwood
- * @Last modified time: 2018-07-03T14:18:01-07:00
- * @Version: 1.0
+ * @Last modified time: 2018-09-24T16:46:33-07:00
+ * @Version: 1.0.1
  */
 
 var keyState = {};
+var keyQueue = [];
 
-//Without both onkeyup and onkeydown, inputting 2 keys really quick won't register one of them
-window.onkeyup = window.onkeydown = function (e){
+//Controls for the snake
+window.onkeypress = function (e){ // Action based on keydown
   e = e || event;
   keyState[e.keyCode] = e.type == 'keydown';
   e.preventDefault();
 
+  //key codes dictionary
   var keys = {
     37: 'left',
     38: 'up',
@@ -25,9 +27,11 @@ window.onkeyup = window.onkeydown = function (e){
     27: 'restart'
   }
 
+
   var dx = 0;
   var dy = 0;
 
+  // key detection and sets new speed (direction)
   switch (keys[e.keyCode]){
     case 'left':
       dx = -1;
@@ -45,17 +49,31 @@ window.onkeyup = window.onkeydown = function (e){
       console.log("WOW");
       render();
       init();
+      integrate();
       break;
     default:
       return;
   }
 
-
+  /* If the new direction is in the x-axis and the direction is already moving in the x direction,
+  * then there will be an issue. The issue is, for example, if the snake is moving down, but the command
+  * is to go up, then it intersects with itself and the user will lose.
+  */
   if(dx*speed.x != 0 || dy * speed.y != 0){
     return;
   }
+
+
+  //Instead of using speed variable, we use new speed just so that there will not be any conflicts
+  // during comparison in the above function. The values might be the same if we change speed and then compare
+  // it with itself.
+
   newSpeed = {
     x: dx,
     y: dy
   };
+
+  keyQueue.push(newSpeed);
+  console.log("test")
+
 };
--- render.js
+++ render.js
@@ -1,54 +1,64 @@
 /**
  * Visuals for the HTML5 game
+ * Render can access snake.js but snake.js can't access render.js
  *
  * @Author: Daniel Forwood
  * @Date:   2018-06-29T19:13:13-07:00
  * @Last modified by:   Daniel Forwood
- * @Last modified time: 2018-07-02T23:31:18-07:00
- * @Version: 1.0
+ * @Last modified time: 2018-09-24T19:13:44-07:00
+ * @Version: 1.0.1
  */
 
 
 
+// Getting tag from HTML. Puts display in here.
 var canvas = document.getElementById('canvas');
-var W = 600, H = 600;
-var BOX_W = W/COLS;
-var BOX_H = H/ROWS;
+
+var W = 600, H = 600; // Size of canvas
+var BOX_W = W/COLS; // Pixels per box width
+var BOX_H = H/ROWS; // Pixels per box height
 var ctx = canvas.getContext('2d');
-var fruit_image = new Image(BOX_W,BOX_H);
+var fruit_image = new Image(BOX_W,BOX_H); //Creates an Image variable of size Box_W & Box_H
+fruit_image.src = 'Icons/apple.png'; //Picture for the fruit
 
 
-
+//Transforms from COLS to x axis pixel
 function transformX(x){
   return W * x / COLS;
 }
 
+//Transforms from ROW to y axis pixel
 function transformY(y){
   return H * (1-(y+1) / ROWS);
 }
 
+//Renders the snake on the canvas
 function renderSnake(){
 
-  var snakeHeadx = snake[0].x;
-  var snakeHeady = snake[0].y;
+  var snakeHeadx = snake[0].x; // Column that snake head is in
+  var snakeHeady = snake[0].y; // Row that snake head is in
 
   var xx2 = transformX(snakeHeadx),
       yy2 = transformY(snakeHeady);
 
-  ctx.fillStyle = "#FFA500";
-  ctx.fillRect(xx2,yy2,BOX_W,BOX_H);
-  ctx.strokeStyle = 'black';
-  ctx.strokeRect(xx2, yy2, BOX_W, BOX_H);
 
 
-  for(var i = 1; i < snake.length; ++i){
-    var x = snake[i].x,
-        y = snake[i].y;
+
+  ctx.fillStyle = "#FFA500"; //Set head to orange
+  ctx.fillRect(xx2,yy2,BOX_W,BOX_H); // Draw head
+  ctx.strokeStyle = 'black'; //Set border to black
+  ctx.strokeRect(xx2, yy2, BOX_W, BOX_H); //Draw border
+
+
+  // Draws the snake
+  for(var i = 1; i < snake.length; ++i){  //Every index in snake is part of the snake
+    var x = snake[i].x,  // x-value location of snake head & body
+        y = snake[i].y; // y-value location of snake head & body
 
     var xx = transformX(x),
         yy = transformY(y);
 
-
+    // If still playing, set color of the snake is brown. If game is lost, set color of the snake is red
     if(playing){
       ctx.fillStyle = '#A26A42';
     }
@@ -56,32 +66,24 @@ function renderSnake(){
       ctx.fillStyle = 'red';
     }
 
-    ctx.fillRect(xx, yy, BOX_W, BOX_H);
-    ctx.strokeStyle = 'black';
-    ctx.strokeRect(xx, yy, BOX_W, BOX_H);
+    ctx.fillRect(xx, yy, BOX_W, BOX_H); //Draws 1 single box of the snake with setted color
+    ctx.strokeStyle = 'black'; //Set border color to black
+    ctx.strokeRect(xx, yy, BOX_W, BOX_H); //Draws the border
+
   }
-
-
 }
+
 
 function renderFruit(){
-  var xx3 = transformX(fruit.x);
-  var yy3 = transformY(fruit.y);
-
-
-  fruit_image.src = 'Icons/apple.png';
+  var xx3 = transformX(fruit.x); //transforms from COLS to x pixel values
+  var yy3 = transformY(fruit.y); //transforms from ROWS to y pixel values
 
   fruit_image.onload = function(){
-    ctx.drawImage(fruit_image,xx3,yy3,BOX_W+5,BOX_H+5)
-  }
-  console.log("HI");
-  //console.log(playing);
-
-  //ctx.fillStyle = 'green';
-  //ctx.fillRect(xx,yy, BOX_W, BOX_H);
-  //ctx.strokeSytyle = 'black';
-  //ctx.strokeRect(xx,yy, BOX_W, BOX_H);
+      ctx.drawImage(fruit_image,xx3-2,yy3-2,BOX_W+3,BOX_H+3)  //(image, x-pos,y-pos,x-size,y-size) x-pos and y-pos are offset to centre
+    }
+    fruit_image.src = 'Icons/apple.png'; //Picture for the fruit
 }
+
 
 function init(){
   renderSnake();
@@ -91,13 +93,11 @@ function init(){
 
 function render(){
   if(playing){
-    ctx.clearRect(0,0,W,H);
+    ctx.clearRect(0,0,W,H);   //clears the canvas
+    renderSnake();   //draws the snake
+    renderFruit(); //draws the fruit
 
-    renderSnake();
-
-    renderFruit();
-
-    setTimeout(render, 20);
+    setTimeout(render, 50); //calls render every 20ms
   }
   renderSnake();
 }
